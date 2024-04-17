2e4740f54bebd3b7c1b71a3666a47b0bfb43ec31
laurennguyen <lbn53748@creighton.edu>
2024-04-17 14:05:00 -0500

    Added view bones.

--- myproject/myapp/admin.py
+++ myproject/myapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import User, Post, Comment, Poll, PollOption, PollVote, Like
 
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Poll)
+admin.site.register(PollOption)
+admin.site.register(PollVote)
+admin.site.register(Like)
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import generic
+from.models import Polls
 
 # Create your views here.
+class PostView():
+    # model = x (add a html model file with css shit)
+    template_name = ''
+
+class CreatePostView():
+    #same as above
+    template_name = ''
+
+class CommentView():
+    #^^
+    template_name = ''
